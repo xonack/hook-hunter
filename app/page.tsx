3dbf4a597c89b17fa165de882198768b3e401d8c
xonack <jenshonack@gmail.com>
2024-10-06 20:06:28 +0100

    refine

--- app/page.tsx
+++ app/page.tsx
@@ -75,7 +75,7 @@ export default function Home() {
       <h1 className="text-6xl font-bold mb-32 text-center">Hook Hunter</h1>
       <div className="flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0 justify-center items-end mb-8">
         <Input
-          placeholder="List ID"
+          placeholder="X List ID"
           value={listID}
           onChange={(e) => setListID(e.target.value)}
           className="md:w-1/4"
